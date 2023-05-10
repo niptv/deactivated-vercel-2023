c457b2cd03238e97d80bf3c9af8b49d738b2b4b8
niptv <98809395+niptv@users.noreply.github.com>
2023-05-10 11:05:18 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -249,7 +249,7 @@ export default function Home() {
           <Grid.Row>
             <Grid.Column></Grid.Column>
             <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-              <a href="https://github.com/saifshaikh1805/tata-sky-m3u" target="_blank" rel="noreferrer">View source code on Github</a>
+              <a href="https://www.google.com" target="_blank" rel="noreferrer">View source code on Github</a>
             </Grid.Column>
             <Grid.Column></Grid.Column>
           </Grid.Row>
